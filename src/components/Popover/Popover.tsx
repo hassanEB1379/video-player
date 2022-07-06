0cfe07b4a75ecd3cd05c24263ea7bc4d53b4aff3
hassan <hassanebrahimi560@gmail.com>
2022-07-06 13:11:57 +0430

    create popover component

---
+++ src/components/Popover/Popover.tsx
@@ -0,0 +1,36 @@
+import React, {useRef, useState} from 'react';
+import {useWhenClickOutside} from '../../hooks/useWhenClickOutside';
+
+interface Props {
+    trigger: (open: React.MouseEventHandler) => React.ReactNode,
+    children: React.ReactNode,
+    className?: string
+}
+
+const Popover = ({trigger, children, className}: Props) => {
+    const ref = useRef<HTMLDivElement>();
+
+    const [open, setOpen] = useState(false);
+
+    const handleOpen = () => setOpen(true);
+
+    const handleClose = () => setOpen(false);
+
+    useWhenClickOutside([ref.current], () => {
+        handleClose();
+    })
+
+    return (
+        <div
+            ref={ref}
+            style={{position: 'relative'}}
+            className={className}
+        >
+            {trigger(handleOpen)}
+
+            {open && <div style={{position: 'absolute', bottom: '110%', right: 0}}>{children}</div>}
+        </div>
+    );
+};
+
+export default Popover;
