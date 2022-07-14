d0b8b35a46addb070b7260a7e7d410185ceb4f15
hassan <hassanebrahimi560@gmail.com>
2022-07-14 22:05:18 +0430

    change markdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,15 @@
-import React, {ChangeEvent, useRef, useState} from 'react';
+import React from 'react';
 import VideoPlayer from './components/VideoPlayer/VideoPlayer';
-import Button from './components/Button/Button';
 import {IconContext} from 'react-icons';
 
 import 'regenerator-runtime/runtime.js';
 import './assets/css/normalize.css'
 import './assets/css/global.css'
-import styles from './styles/app.module.css'
 
 const App = () => {
-    const [src, setSrc] = useState('');
-
-    const ref = useRef<HTMLInputElement>(null);
-
-    const handleOpenFileExplorer = () => {
-        ref.current.click();
-    }
-
-    const handleSelectFile = (e: ChangeEvent<HTMLInputElement>) => {
-        const file = e.target.files[0];
-
-        setSrc(URL.createObjectURL(file))
-    }
-
     return (
-        <IconContext.Provider value={{color: 'white', size: '2rem'}}>
-            <div className={styles.container}>
-                {src ?
-                    <VideoPlayer src={src}/>:
-                    <div className={styles.dropzone}>
-                        <Button onClick={handleOpenFileExplorer}>Choose file</Button>
-
-                        <input
-                            onChange={handleSelectFile}
-                            ref={ref}
-                            name='choose-file'
-                            type='file'
-                            accept='mkv,mp4'
-                            className={styles.input}
-                        />
-                    </div>}
-            </div>
+        <IconContext.Provider value={{color: 'white', size: '1.2rem'}}>
+            <VideoPlayer />
         </IconContext.Provider>
     );
 };
