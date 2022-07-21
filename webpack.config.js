a4898b0007d457357636e14af9bc4c381787b6c1
hassan <hassanebrahimi560@gmail.com>
2022-07-21 17:43:59 +0430

    fix template src

--- webpack.config.js
+++ webpack.config.js
@@ -58,7 +58,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './public/index.html'
+            template: './src/index.html'
         }),
     ],
     optimization: {},
