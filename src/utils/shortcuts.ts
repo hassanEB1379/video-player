13021bd7dc9521ed2cc7d8daf0ac620752a65700
hassan <hassanebrahimi560@gmail.com>
2022-07-27 22:09:11 +0430

    add shortcuts for change volume

--- src/utils/shortcuts.ts
+++ src/utils/shortcuts.ts
@@ -1,5 +1,7 @@
 export const shortcuts = {
     GO_FURTHER: 'ArrowRight',
     GO_BACK: 'ArrowLeft',
-    FULLSCREEN_TOGGLE: 'alt+Enter'
+    FULLSCREEN_TOGGLE: 'alt+Enter',
+    INCREASE_VOLUME: 'ArrowUp',
+    DECREASE_VOLUME: 'ArrowDown'
 }
