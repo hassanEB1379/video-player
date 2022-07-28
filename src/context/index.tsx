83bf5311ff0ebd3ccca980175774af8c5aa5593f
hassan <hassanebrahimi560@gmail.com>
2022-07-28 16:42:58 +0430

    create context for show messages

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -3,16 +3,19 @@ import RecentVideosProvider from './VideoPlayer/RecentVideos';
 import ReactIcon from './ReactIcon';
 import VideoSrcProvider from './VideoPlayer/VideoSrc';
 import VolumeProvider from './VideoPlayer/Volume';
+import VideoMessageProvider from './VideoPlayer/Message';
 
 const Providers = ({children}: {children: React.ReactNode}) => {
     return (
         <RecentVideosProvider>
             <VideoSrcProvider>
-                <VolumeProvider>
-                    <ReactIcon>
-                        {children}
-                    </ReactIcon>
-                </VolumeProvider>
+                <VideoMessageProvider>
+                    <VolumeProvider>
+                        <ReactIcon>
+                            {children}
+                        </ReactIcon>
+                    </VolumeProvider>
+                </VideoMessageProvider>
             </VideoSrcProvider>
         </RecentVideosProvider>
     );
