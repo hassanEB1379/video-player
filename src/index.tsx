b74ef8de830089bb411051c28232645c3a770ee4
hassan <hassanebrahimi560@gmail.com>
2022-08-02 20:12:40 +0430

    extract service worker register logic to separate file

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
 import App from './App';
+import register from './service-worker-register';
 
-// @ts-ignore
 const root = ReactDom.createRoot(document.getElementById('root'));
 
 root.render(<App/>)
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/video-player/service-worker.js')
-            .then(registration => {
-                console.log('SW registered: ', registration);
-            }).catch(registrationError => {
-                console.log('SW registration failed: ', registrationError);
-            });
-    });
-}
+register();
---
+++ src/service-worker-register.ts
@@ -0,0 +1,12 @@
+export default function () {
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+            navigator.serviceWorker.register('/video-player/service-worker.js')
+                .then(registration => {
+                    console.log('SW registered: ', registration);
+                }).catch(registrationError => {
+                    console.log('SW registration failed: ', registrationError);
+                });
+        });
+    }
+}
