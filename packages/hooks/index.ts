bd0a0a2c7e77ca86e8d255f0dcc56ec3825c4ae9
hassan <hassanebrahimi560@gmail.com>
2022-08-04 21:48:04 +0430

    remove old files

--- packages/hooks/index.ts
+++
@@ -1,3 +0,0 @@
-export {useShortcut} from './useShortcut'
-export {useFullscreen} from './useFullscreen'
-export {useWhenClickOutside} from './useWhenClickOutside'
--- packages/utils/index.ts
+++
@@ -1,4 +0,0 @@
-export {srt2vtt} from './srt2vtt'
-export {getFormattedTime} from './getFormattedTime'
-export {shortcuts} from './shortcuts'
-export {toggleFullscreen} from './toggleFullscreen'
