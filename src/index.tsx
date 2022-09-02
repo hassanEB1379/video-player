4894f41ed06093bd731943e369c3ed31e53da394
hassan <hassanebrahimi560@gmail.com>
2022-09-02 16:18:26 +0430

    migrate to vite

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
 import App from './App';
-import register from './service-worker-register';
 
 const root = ReactDom.createRoot(document.getElementById('root'));
 
 root.render(<App/>)
-
-register();
---
+++ vite.config.ts
@@ -0,0 +1,44 @@
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+import react from '@vitejs/plugin-react'
+
+const pwa = VitePWA({
+    registerType: 'autoUpdate' ,
+    devOptions: {
+        enabled: true
+    },
+    manifest: {
+        'name':'Video player',
+        'short_name':'Video player',
+        'icons':[
+            {
+                'src':'/images/android-chrome-192x192.png',
+                'sizes':'192x192',
+                'type':'image/png'
+            },
+            {
+                'src':'/images/android-chrome-512x512.png',
+                'sizes':'512x512',
+                'type':'image/png'
+            }
+        ],
+        'theme_color':'#06283D',
+        'background_color':'#06283D',
+        'display':'standalone',
+        'start_url': '.',
+        'id': '/video-player'
+    }
+})
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react(), pwa],
+    server: {
+        port: 9000,
+        host: 'localhost',
+        https: {
+            key: './localhost-key.pem',
+            cert: './localhost.pem',
+        }
+    }
+})
